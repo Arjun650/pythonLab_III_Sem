9a81e767ce8c4944e4de137e0685c8ca7d3eb2bf
Arjun Das <darjun650@gmail.com>
2023-12-20 20:49:25 +0530

    8

---
+++ qus8.py
@@ -0,0 +1,17 @@
+# Write code to produce a star triangle in Python.
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+n = eval(input("Enter the number of row"))
+
+for i in range(1 , n+1):
+    
+    for j in range(1 , n-i +1 ):
+        print(" " , end=" ")
+    
+    for k in range(1 , ((2 * i) - 1) + 1):
+        print("*" , end = " ")
+    print()
---
+++ qus9.py
@@ -0,0 +1,4 @@
+# Given a list, write a Python program to swap first and last element of the list.
+
+n = int(input("Enter the length of the list"))
+myList = []
