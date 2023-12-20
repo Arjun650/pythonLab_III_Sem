b8106e108c060f24f6b576a37a9e34ea373423f9
Arjun Das <darjun650@gmail.com>
2023-12-20 20:55:54 +0530

    add

--- qus9.py
+++ qus9.py
@@ -2,3 +2,15 @@
 
 n = int(input("Enter the length of the list"))
 myList = []
+def letSwap(myList):
+    temp = myList[0]
+    myList[0] = myList[-1]
+    myList[-1] = temp
+
+for i in range(n):
+    ele = eval(input())
+    myList.append(ele)
+
+letSwap(myList)
+for j in myList:
+    print (j)
